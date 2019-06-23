8f4f578e6c2c5413e22b5ec656fb6fbb1dcb59ab
Lucas Atsuo Ito <lucasatsuo@gmail.com>
2019-06-23 17:18:57 -0300

    funcionalidade auto da temperatura

--- src/app/components/temperatura-card/temperatura-card.component.ts
+++ src/app/components/temperatura-card/temperatura-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { TemperaturaService } from 'src/app/services/temperatura.service';
 
 @Component({
   selector: 'app-temperatura-card',
@@ -8,13 +9,13 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TemperaturaCardComponent implements OnInit {
 
   @Input() temperatura={};
-  constructor() { }
+  constructor(private temperaturaService: TemperaturaService) { }
 
   ngOnInit() {
   }
   toggleTempAuto(){
     this.temperatura["auto"] = !this.temperatura["auto"]
-    //comando para atualizar no banco 
+    this.temperaturaService.putTemperatura(this.temperatura["id"],this.temperatura).subscribe((data)=>{},erro=>console.log(erro))
   }
 
 }
