434e665aac3dc46031cd860e2fe4dcc71acca520
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-06-26 18:33:38 -0300

    cria baia persistente

--- src/app/pages/cria-baia/cria-baia.component.ts
+++ src/app/pages/cria-baia/cria-baia.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BaiaService } from 'src/app/services/baia.service';
 
 @Component({
   selector: 'app-cria-baia',
@@ -16,14 +18,56 @@ export class CriaBaiaComponent implements OnInit {
     iluminacao: new FormControl('')
   });
 
-  constructor() {
+  constructor(private router: Router, private baiaService: BaiaService) {
   }
 
   ngOnInit() {
   }
 
   onSubmit() {
-    console.log(this.criaBaiaForm.value);
+    let input = this.criaBaiaForm.value;
+
+    let baiaCompleta = {
+      name: input.name,
+      age: input.age,
+      agua: {
+        nivel: 100,
+        nivelSet: parseInt(input.aguaSet),
+        data: null,
+        descricao: "sensor água",
+        estado: true,
+        auto: true,
+        ph: 7.8
+      },
+      alimento: {
+        nivel: 100,
+        nivelSet: parseInt(input.comidaSet),
+        data: null,
+        descricao: "sensor comida",
+        estado: true,
+        auto: true
+      },
+      temperatura: {
+        currentTemperature: parseInt(input.temperaturaSet),
+        temperatureSet: parseInt(input.temperaturaSet),
+        data: null,
+        descricao: "sensor temperatura",
+        estado: true,
+        auto: true
+      },
+      luminosidade: {
+        luminosidade: (input.iluminacao == 0) ? 'false' : 'true',
+        auto: true,
+        data: null,
+        descricao: "sensor iluminacao",
+        estado: true
+      },
+      status: true
+    };
+
+    console.log(baiaCompleta);
+    this.baiaService.postBaia(baiaCompleta).subscribe(data => console.log(data));
+    this.router.navigate(['/home']);
   }
 
 }
